7534a7235804c136303e8221c8e6c78b92233d46
Patrick <patrickpierre2015@gmail.com>
2024-03-13 21:11:28 -0300

    atualizando repositorio nuvem

--- admin/index.php
+++ admin/index.php
@@ -60,31 +60,42 @@ session_start();
 <body id="page-top">
     <?php
     require "funcoes.php";
+    // verificando se a sessão usuario existe
     $usuarioNaoExiste = !isset($_SESSION["usuario"]);
+
+    // se o usuario nao existir entra no if e é encaminhado pro login
     if ($usuarioNaoExiste) {
         //Puxa o trecho de login php para dentro do IF
         require "paginas/login.php";
     } else {
+        // verifica se existe algum parametro dentro da variavel global GET
         if (isset($_GET['param'])) {
+
             // 'paginas/home/1
             // paginas == pasta/diretorio
             // home = o arquivo php
             // $array['paginas','home','id']
 
+            // explode está desmontando o array param, dividindo pela "/" 
             $page = explode("/", $_GET['param']);
 
+            // adicionando as partes desmontadas em cada variável
             $pasta = $page[0] ?? NULL;
             $arquivo = $page[1] ?? NULL;
             $id = $page[2] ?? NULL;
 
+            // montando a url da pagina
             $page = "$pasta/$arquivo";
 
+            require "header.php";
+            // verificando se existe o arquivo com o nome da variavel page
             if(file_exists("$page.php")) {
                 require "$page.php";
             } else {
+                // caso nao exista um arquivo com o nome page redireciona o usuario para uma pagina com mensagem de erro!
                 require "paginas/error.php";
             }
-           
+            require "footer.php";
         }
     }
     ?>
--- admin/paginas/error.php
+++ admin/paginas/error.php
@@ -1,1 +1,3 @@
-<h1>erro</h1>
+<div class="text-center">
+    <img src="images/erro.png" alt="Página não encontrada" class="w-100">
+</div>
